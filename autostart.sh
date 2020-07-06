d5e8ca97871e880902d335c304ee9d555ec9be98
Creator54 <hi.creator54@gmail.com>
2020-07-06 11:22:42 +0530

    moved most services to xinit

--- autostart.sh
+++ autostart.sh
@@ -18,14 +18,14 @@ done
 
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
-run "nm-applet"
+#run "nm-applet"
 #run "caffeine"
 #run "xfce4-power-manager"
 #run "blueberry-tray"
-run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-run "numlockx on"
+#run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+#run "numlockx on"
 #run "volumeicon"
-run "nitrogen --restore"
+#run "nitrogen --restore"
 #run "conky -c $HOME/.config/awesome/system-overview"
 
 #run applications from startup
