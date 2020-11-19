c6ab4874b33f544e0ff18e5375e0d8d11ac24e6f
creator54 <hi.creator54@gmail.com>
2020-11-19 21:32:48 +0530

    fix clock

--- themes/powerarrow-blue/theme.lua
+++ themes/powerarrow-blue/theme.lua
@@ -105,7 +105,7 @@ local separators = lain.util.separators
 -- Textclock
 local clockicon = wibox.widget.imagebox(theme.widget_clock)
 local clock = awful.widget.watch(
-    "date +'%a %d %b %R'", 60,
+    "date +'%a %d %b %r'", 1,
     function(widget, stdout)
         widget:set_markup(" " .. markup.font(theme.font, stdout))
     end
