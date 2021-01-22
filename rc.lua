947c40c5b846c411da65ad34a2e7fa31b00338b9
creator54 <hi.creator54@gmail.com>
2021-01-22 12:36:46 +0530

    add border width

--- rc.lua
+++ rc.lua
@@ -447,7 +447,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 6,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
