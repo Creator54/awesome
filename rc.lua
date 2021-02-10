7237f4b80d6bc132ad32cfd4eae4e32248fa2e74
creator54 <hi.creator54@gmail.com>
2021-02-10 07:40:28 +0530

    remove depreceated stuff

--- rc.lua
+++ rc.lua
@@ -498,8 +498,6 @@ globalkeys = my_table.join(
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift" }, "w", function () awful.spawn.with_shell("feh --bg-fill --randomize ~/wallpapers/") end,
               {description = "refresh wallpaper", group ="awesome"}),
-    awful.key({ modkey, "Shift" }, "q", function () awful.spawn.with_shell(" killall autostart.sh") end,
-              {description = "killall autostart.sh processes", group = "hotkeys"}),
     awful.key({ modkey, "Shift" }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
