6e30f036da97c40f0cf9c6ccc00736c8f7c09622
creator54 <hi.creator54@gmail.com>
2021-07-22 08:40:55 +0530

    open nnn by default

--- rc.lua
+++ rc.lua
@@ -49,7 +49,7 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.font = "Droid Sans Bold 10"
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
+terminal = "kitty -e 'nnn'"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
